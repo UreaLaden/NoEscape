52b71b3eea7ef5aa2f070706b0473c5d8ca52a2f
Leaundrae Mckinney <pytechdrae@gmail.com>
2021-07-21 11:46:12 -0400

    Added flashlight

--- Assets/Scripts/LightSettings.cs
+++ Assets/Scripts/LightSettings.cs
@@ -8,7 +8,10 @@ public class LightSettings : MonoBehaviour
     [SerializeField] private PostProcessVolume _postProcessVolume;
     [SerializeField] private PostProcessProfile[] _postProcessProfiles;
     [SerializeField] private GameObject nightVisionUI;
+    [SerializeField] private GameObject flashLight;
     private bool _nightVisionActive = false;
+
+    private bool _flashlightActive = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,9 +23,15 @@ public class LightSettings : MonoBehaviour
     {
         _postProcessVolume.profile = _nightVisionActive ? _postProcessProfiles[1] : _postProcessProfiles[0];
         nightVisionUI.SetActive(_nightVisionActive);
+        flashLight.SetActive(_flashlightActive);
         if (Input.GetKeyUp(KeyCode.N))
         {
             _nightVisionActive = !_nightVisionActive;
+        }
+
+        if (Input.GetKeyUp(KeyCode.F))
+        {
+            _flashlightActive = !_flashlightActive;
         }
     }
 }
